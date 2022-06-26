30c97d9327c6c6aeb0ce81bf8daf1a4bda3e903e
KeyurMistry <keyurmist@gmail.com>
2022-06-26 20:48:31 +0100

    added middleware

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
+
+app.use(express.static("public"));
