dcc2c3f4e93b4f0f59068a1fcbce096e36ae7ab4
KeyurMistry <keyurmist@gmail.com>
2022-06-26 20:57:33 +0100

    added GET functions for index and notes html

---
+++ routes/html.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
