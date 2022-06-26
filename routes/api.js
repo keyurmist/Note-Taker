c018e5ab9c252b19a6fd7a890f631c3d615ee00e
KeyurMistry <keyurmist@gmail.com>
2022-06-26 21:36:28 +0100

    added GET to api.js to grab note list

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,8 @@
 const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
+const api = require("express").Router();
+
+api.get("/notes", (req, res) => {
+  res.json("../db/db.json");
+});
