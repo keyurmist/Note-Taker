4ebad58e9dcfc6a04590a8c8ad86cf30031a7954
KeyurMistry <keyurmist@gmail.com>
2022-07-07 13:29:53 +0100

    altered path to string so save works

--- routes/api.js
+++ routes/api.js
@@ -5,36 +5,36 @@ const { v4: uuidv4 } = require("uuid");
 const db = require("../db/db.json");
 
 api.get("/notes", (req, res) => {
-  res.json(notes);
+  res.sendFile(path.join(__dirname, "../db/db.json"));
 });
 
 api.post("/notes", (req, res) => {
+  const newNote = req.body;
   console.log(req.body);
 
-  const newNote = req.body;
   newNote.id = uuidv4();
 
-  let data = JSON.parse(fs.readFileSync(db, "utf8"));
+  let data = JSON.parse(fs.readFileSync("./db/db.json"));
 
   data.push(newNote);
 
-  fs.writeFileSync(notes, JSON.stringify(data));
+  fs.writeFileSync("./db/db.json", JSON.stringify(data));
 
   console.log("Successfully added new note");
 
   res.json(data);
 });
 
-api.delete("/notes/:id", (req, res) => {
-  let deleteNote = request.params.id.toString();
+// api.delete("/notes/:id", (req, res) => {
+//   let deleteNote = request.params.id.toString();
 
-  let data = JSON.parse(fs.readFileSync(db, "utf8"));
+//   let data = JSON.parse(fs.readFileSync(db, "utf8"));
 
-  const newData = data.filter((note) => note.id.toString() !== deleteNote);
+//   const newData = data.filter((note) => note.id.toString() !== deleteNote);
 
-  fs.writeFileSync(db, JSON.stringify(newData));
+//   fs.writeFileSync(db, JSON.stringify(newData));
 
-  res.json(newData);
-});
+//   res.json(newData);
+// });
 
 module.exports = api;
--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-//middleware
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use("/api", api);
 // app.use("/html", html);
-app.use("./routes/api", api);
-app.use("./routes/html", html);
 
 app.use(express.static("public"));
+app.use("/api", api);
+app.use("/", html);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
