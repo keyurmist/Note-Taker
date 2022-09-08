ad48bbeb0388b68f53fac597883510fbcf14acca
KeyurMistry <keyurmist@gmail.com>
2022-09-08 12:01:19 +0100

    debugged typo

--- routes/html.js
+++ routes/html.js
@@ -6,7 +6,7 @@ html.get("/notes", (req, res) =>
 );
 
 html.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "../public.index.html"))
+  res.sendFile(path.join(__dirname, "../public/index.html"))
 );
 
 module.exports = html;
